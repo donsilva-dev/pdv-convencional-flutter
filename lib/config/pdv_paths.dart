630b78db0770cc8c699ddf1ce57f88f2f24c2e24
Douglas Nunes Silva <douglas.n.silva30@gmail.com>
2026-08-31 01:56:04 -0300

    Ajusta paths Windows e Linux

--- lib/config/pdv_paths.dart
+++ lib/config/pdv_paths.dart
@@ -1,7 +1,35 @@
+// class PdvPaths {
+//   static const String root = r'D:\vmix\vmix';
+
+//   static const String parametros = r'D:\vmix\vmix\param\parametros_pdv.xml';
+
+//   static const String pictures = r'D:\vmix\vmix\pictures';
+// }
+
+import 'dart:io';
+
 class PdvPaths {
-  static const String root = r'D:\vmix\vmix';
+  static String get root {
+    if (Platform.isWindows) {
+      return r'D:\vmix\vmix';
+    }
 
-  static const String parametros = r'D:\vmix\vmix\param\parametros_pdv.xml';
+    return '/vmix/vmix';
+  }
 
-  static const String pictures = r'D:\vmix\vmix\pictures';
+  static String get parametros {
+    if (Platform.isWindows) {
+      return r'D:\vmix\vmix\param\parametros_pdv.xml';
+    }
+
+    return '/vmix/vmix/dataisp/linux/param/parametros_pdv.xml';
+  }
+
+  static String get pictures {
+    if (Platform.isWindows) {
+      return r'D:\vmix\vmix\pictures';
+    }
+
+    return '/vmix/vmix/pictures';
+  }
 }
